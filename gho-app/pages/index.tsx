9d021cfe53d7727026f1d8f5c503c95bf3028044
Harrisistaken <132925337+Harrisistaken@users.noreply.github.com>
2024-01-14 21:17:52 +0530

    .

--- gho-app/pages/index.tsx
+++ gho-app/pages/index.tsx
@@ -1,22 +1,13 @@
 // pages/index.tsx
 import React from 'react';
-import { WagmiConfig, createConfig } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
-import {
-  ThirdwebProvider,
-} from "@thirdweb-dev/react";
-import MyComponent from './MyComponent';
+import { ConnectKitButton } from 'connectkit';
 
-const config = createConfig({
-  chains: [mainnet, polygon, optimism, arbitrum],
-  walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-});
 
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Connect Wallet Demo</h1>
-          <MyComponent />
+      <ConnectKitButton/>
     </div>
   );
 };
