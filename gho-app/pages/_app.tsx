1228133036de343c2c6c99eea74d642283df4365
KumaraKabilan <kumarakabilan2004@gmail.com>
2024-01-15 20:06:50 +0530

    pushh

--- gho-app/pages/_app.tsx
+++ gho-app/pages/_app.tsx
@@ -5,9 +5,33 @@ import { WagmiConfig, createConfig } from 'wagmi';
 import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
 import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
 
+
 import {
   ThirdwebProvider,
+  ConnectWallet,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+  safeWallet,
+  trustWallet,
+  zerionWallet,
+  bloctoWallet,
+  magicLink,
+  frameWallet,
+  rainbowWallet,
+  phantomWallet,
+  smartWallet,
+  embeddedWallet,
+  en,
+  localWallet,
 } from "@thirdweb-dev/react";
+
+const activeChain = "mumbai";
+
+const smartWalletOptions = {
+  factoryAddress: "0x1511d790e8006acaf26b74d2e22febf357f3a4b0",
+  gasless: true,
+};
 
 const config = createConfig(
   getDefaultConfig({
@@ -22,7 +46,35 @@ const config = createConfig(
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      clientId="cd53e0c74ffcf66da0d8098ee483c4fc">
+      clientId="cd53e0c74ffcf66da0d8098ee483c4fc" locale={en()}
+      supportedWallets={[
+        smartWallet(
+          coinbaseWallet(),
+          smartWalletOptions,
+        ),
+        smartWallet(
+          walletConnect(),
+          smartWalletOptions,
+        ),
+        smartWallet(
+          localWallet(),
+          smartWalletOptions,
+        ),
+        smartWallet(
+          embeddedWallet({
+            auth: {
+              options: [
+                "email",
+                "google",
+                "apple",
+                "facebook",
+              ],
+            },
+          }),
+          smartWalletOptions,
+        ),
+      ]}
+    >
     <WagmiConfig config={config}>
       <ConnectKitProvider debugMode>
         <Component {...pageProps} />
