3b667d67ee6edb1a2f6b6fadfe1433f57061c3b3
Kabilan <kumarakabilan2004@gmail.com>
2024-01-20 23:23:47 +0530

    push

--- front-end/ghoan/components/card.tsx
+++ front-end/ghoan/components/card.tsx
@@ -1,19 +1,34 @@
 // components/Card.tsx
 
-import { ConnectEmbed } from "@thirdweb-dev/react";
+import { ConnectEmbed, useAddress, useShowConnectEmbed } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 import Image from 'next/image';
 
+const loginOptional = false;
+
 const Card = () => {
+  const address = useAddress();
+  const showConnectEmbed = useShowConnectEmbed(loginOptional);
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
         <div className={styles.centeredContainer}>
           <div className={styles.cardEmbed}>
             <div className={styles.cardImage}>
-              <Image src="/images/Card.png" alt="IMAGE" width={412} height={251} />
+              <Image src="/images/cardBig.png" alt="IMAGE" width={412} height={251}  />
               <div className={styles.connectEmbed}>
-                <ConnectEmbed />
+                {showConnectEmbed ? (
+                  <ConnectEmbed
+                    auth={{
+                      loginOptional: loginOptional,
+                    }}
+                  />
+                ) : (
+                  <div className={styles.connectedAddress}>
+                   <h1> Connected Wallet :</h1> {address}
+                  </div>
+                )}
               </div>
             </div>
           </div>
