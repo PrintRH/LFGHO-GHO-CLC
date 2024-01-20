3cd8f50a4931cce05873ce5ff3c6912f373df321
Kabilan <kumarakabilan2004@gmail.com>
2024-01-20 16:37:53 +0530

    Pushh

--- front-end/ghoan/pages/_app.tsx
+++ front-end/ghoan/pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider, embeddedWallet, metamaskWallet, smartWallet, localWallet } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-import { ACCOUNT_FACTORY_ADDRESS } from "../constants/addresses";
 
 
 // This is the chain your dApp will work on.
@@ -9,20 +8,20 @@ import { ACCOUNT_FACTORY_ADDRESS } from "../constants/addresses";
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
 const activeChain = "mumbai";
 
+const personalWallet = embeddedWallet(); // or any other wallet
+
+export const smartWalletConfig = smartWallet(personalWallet, {
+  factoryAddress: "0x4E6b6f3dfF9faC105BCC55A48A4c2C8941cfaf97",
+  gasless: true,
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  const smartWalletConfig = {
-    factoryAddress: ACCOUNT_FACTORY_ADDRESS,
-    gasless: true,
-  }
 
   return (
     <ThirdwebProvider
-      clientId="9516e58d95cc8d72ae67816057f1049c"
+      clientId="9421a2b8bea44ad14c4184900860cc94"
       activeChain={activeChain}
-      supportedWallets={[
-        smartWallet(embeddedWallet(), smartWalletConfig),
-        smartWallet(localWallet(), smartWalletConfig),
-      ]}
+      supportedWallets={[smartWalletConfig]}
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
