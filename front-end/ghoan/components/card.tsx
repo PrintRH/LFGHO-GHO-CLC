538c4740ff48d4aec8fabc690111046e8d6c279a
Kabilan <kumarakabilan2004@gmail.com>
2024-01-20 16:59:01 +0530

    Pushh

--- front-end/ghoan/components/card.tsx
+++ front-end/ghoan/components/card.tsx
@@ -1,16 +1,10 @@
-import { ConnectWallet, Web3Button, embeddedWallet, localWallet, metamaskWallet, smartWallet, useAddress, useConnect, useContract, useOwnedNFTs } from "@thirdweb-dev/react";
+import { ConnectEmbed, Web3Button, embeddedWallet, localWallet, metamaskWallet, smartWallet, useAddress, useConnect, useContract, useOwnedNFTs } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 import { NextPage } from "next";
 import Image from 'next/image';
-import { ACCOUNT_FACTORY_ADDRESS, NFT_CONTRACT_ADDRESS } from "../constants/addresses";
 import { useState } from "react";
 import Button from "./button";
-const embeddedWalletConfig = embeddedWallet();
 
-const smartWalletConfig = smartWallet(embeddedWalletConfig, {
-  factoryAddress: ACCOUNT_FACTORY_ADDRESS,
-  gasless: true,
-});
 
 const Card: NextPage = () => {
 
@@ -18,12 +12,14 @@ const Card: NextPage = () => {
     <main className={styles.main}>
       <div className={styles.container}>
         <div className={styles.centeredContainer}>
+        < ConnectEmbed/>
+
         <Image src="/images/Card.png" alt="IMAGE" width={412} height={251}/>
-        <Image src="/images/Email.png" width={261} height={31} className={styles.email} alt={""}/>
+        {/*<Image src="/images/Email.png" width={261} height={31} className={styles.email} alt={""}/>
         <input className={styles.emailtag}></input>
         <Button onClick={function (): void {
             throw new Error("Function not implemented.");
-          } } label={""}/>
+          } } label={""}/>*/}
         </div>   
       </div>
     </main>
