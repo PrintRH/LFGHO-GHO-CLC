ff79c88a8f54df026508ea6a12e6458d7387a9ec
Kabilan <kumarakabilan2004@gmail.com>
2024-01-20 17:17:11 +0530

    pushh

--- front-end/ghoan/components/card.tsx
+++ front-end/ghoan/components/card.tsx
@@ -1,29 +1,23 @@
-import { ConnectEmbed, Web3Button, embeddedWallet, localWallet, metamaskWallet, smartWallet, useAddress, useConnect, useContract, useOwnedNFTs } from "@thirdweb-dev/react";
+// components/Card.tsx
+
+import { ConnectEmbed } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
-import { NextPage } from "next";
 import Image from 'next/image';
-import { useState } from "react";
-import Button from "./button";
 
-
-const Card: NextPage = () => {
-
+const Card = () => {
   return (
     <main className={styles.main}>
       <div className={styles.container}>
         <div className={styles.centeredContainer}>
           <div className={styles.cardEmbed}>
-          
-
-        <Image src="/images/Card.png" alt="IMAGE" width={412} height={251}/>
-        {/*<Image src="/images/Email.png" width={261} height={31} className={styles.email} alt={""}/>
-        <input className={styles.emailtag}></input>
-        <Button onClick={function (): void {
-            throw new Error("Function not implemented.");
-          } } label={""}/>*/}
-           <ConnectEmbed />
+            <div className={styles.cardImage}>
+              <Image src="/images/Card.png" alt="IMAGE" width={412} height={251} />
+            </div>
+            <div className={styles.connectEmbed}>
+              <ConnectEmbed />
+            </div>
           </div>
-        </div>   
+        </div>
       </div>
     </main>
   );
