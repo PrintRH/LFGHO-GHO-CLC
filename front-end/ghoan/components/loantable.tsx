38a449185f669d82d4afb98d75b0e9de96c179a5
Harrisistaken <132925337+Harrisistaken@users.noreply.github.com>
2024-01-20 16:37:59 +0530

    .

--- front-end/ghoan/components/loantable.tsx
+++ front-end/ghoan/components/loantable.tsx
@@ -20,10 +20,10 @@ const LoanTable: React.FC<LoanTableProps> = ({ rowNames }) => {
         <thead>
           <tr>
             <th style={tableHeaderStyle}></th>
-            <th style={tableHeaderStyle}>Total Supplied</th>
-            <th style={tableHeaderStyle}>Supply API</th>
-            <th style={tableHeaderStyle}>Total Borrows</th>
-            <th style={tableHeaderStyle}>Borrow API (Variable)</th>
+            <th style={tableHeaderStyle}><button className={styles.table1}>Total Supplies</button></th>
+            <th style={tableHeaderStyle}><button className={styles.table1}>Supply API</button></th>
+            <th style={tableHeaderStyle}><button className={styles.table1}>Total Borrows</button></th>
+            <th style={tableHeaderStyle}><button className={styles.table1}>Borrow API</button></th>
           </tr>
         </thead>
         <tbody>
@@ -34,7 +34,7 @@ const LoanTable: React.FC<LoanTableProps> = ({ rowNames }) => {
               onClick={() => handleRowClick(index)}
               className={styles['enlarge-on-hover']} // Use the CSS module class
             >
-              <td style={tableCellStyle}>{rowName}</td>
+              <td style={tableCellStyle}><button className={styles.table1}>{rowName}</button></td>
               <td style={tableCellStyle}>$800K</td>
               <td style={tableCellStyle}>0.01%</td>
               <td style={tableCellStyle}>$0</td>
@@ -48,27 +48,17 @@ const LoanTable: React.FC<LoanTableProps> = ({ rowNames }) => {
 };
 
 const tableHeaderStyle: React.CSSProperties = {
-  padding: '15px',
+  padding: '45px',
   textAlign: 'left',
-  borderTop: '2px solid #009688',
-  borderBottom: '2px solid #009688',
-  borderRight: '1px solid #009688',
-  borderLeft: '1px solid #009688',
   cursor: 'pointer',
 };
 
 const tableCellStyle: React.CSSProperties = {
-  padding: '15px',
+  padding: '45px',
   textAlign: 'left',
-  borderBottom: '1px solid #009688',
-  borderLeft: '1px solid #009688',
-  borderRight: '1px solid #009688',
   cursor: 'pointer',
-  transition: 'background-color 0.3s, transform 0.3s',
+  marginBottom: '100px', // Add vertical space between rows (adjust the value as needed)
 };
 
-const selectedRowStyle: React.CSSProperties = {
-  backgroundColor: '#34495E', // Darker color when row is selected
-};
 
 export default LoanTable;
